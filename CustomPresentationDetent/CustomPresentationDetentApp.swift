6c3ddcab8d469806a3098a3ec9b5c4ac92c6355f
Amarildo Lucas <vmarildo@gmail.com>
2023-03-08 11:42:49 -0300

    Initial Commit

---
+++ CustomPresentationDetent/CustomPresentationDetentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomPresentationDetentApp.swift
+//  CustomPresentationDetent
+//
+//  Created by Amarildo João Custódio Lucas on 08/03/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomPresentationDetentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
